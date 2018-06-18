d29f17ab32947a2c57934663b2698ca3d5d61e58
JiSooMoon <36117303+JiSooMoon@users.noreply.github.com>
2018-06-18 13:43:55 -0400

    evenDigitsOnly.cpp

---
+++ evenDigitsOnly.cpp
@@ -0,0 +1,15 @@
+bool evenDigitsOnly(int n) {
+    
+   
+    
+    while( n > 0){
+      
+        if(n % 2 != 0){
+            return false;
+        }
+        n = n/10;
+    
+    }
+    return true;
+
+}
