0d797e378419155318572c6d02c7b0200668a75e
JiSooMoon <36117303+JiSooMoon@users.noreply.github.com>
2018-06-18 14:29:02 -0400

    Update absoluteValuesSumMinimization.java

--- absoluteValuesSumMinimization.java
+++ absoluteValuesSumMinimization.java
@@ -10,3 +10,9 @@ int absoluteValuesSumMinimization(int[] a) {
         return a[len/2-1];
     }
 }
+
+//The code below is the simplest way to solve this question, based on c++!
+/*int absoluteValuesSumMinimization(std::vector<int> a) {
+    return a[(a.size() - 1) / 2];
+}
+*/
